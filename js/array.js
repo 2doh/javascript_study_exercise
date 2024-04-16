c4a12880acabe0f921d745af566c8e3790a2fca8
2doh <dev.ldh0973@naver.com>
2024-04-16 13:02:43 +0900

    docs : DOM

--- js/array.js
+++ js/array.js
@@ -4,6 +4,7 @@ const numbers = [1, 2, 3, 4, 5];
 // map() 메소드는 원본의 배열을 그대로 두고 새로운 배열을 생성
 // map() 메소드에 매개변수로 설정된 익명함수를 실행하여 얻어진 요소로 구성된 새로운 배열 반환
 let arr = numbers.map((num) => num * num);
+console.log(numbers);
 console.log(arr);
 
 document.getElementById("show").innerHTML = arr;
---
+++ js/dom.js
@@ -0,0 +1,4 @@
+function get_name() {
+  let text = documnet.form1.name.value;
+  alert(text);
+}
