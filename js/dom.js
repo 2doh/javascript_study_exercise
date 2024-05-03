6299997cb522b7ee0a9acd3cdf9d087e09dec64b
2doh <ldh0973@naver.com>
2024-05-03 11:33:59 +0900

    docs : DOM

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
-function changeImg() {
-  document.getElementById("image").src = `images/image.png`;
-}
-function changSize() {
-  document.getElementById("image").width = "1000";
-}
+const elem = document.createElement("p");
+const text = document.createTextNode("변경한 단락");
+
+elem.appendChild(text);
+const p1 = document.getElementById("p1");
+document.getElementById("box").replaceChild(elem, p1);
